8ae0c5a338f2b621afa9bd2c76813b77c34f2850
KostiantynU <115901013+KostiantynU@users.noreply.github.com>
2023-07-16 21:05:16 +0300

    change middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -31,8 +31,7 @@ export const store = configureStore({
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
-      contactsAPI: contactsAPI.middleware,
-    }),
+    }).concat(contactsAPI.middleware),
 });
 
 export const persistor = persistStore(store);
