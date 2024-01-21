9f798afc571733b3337a2924272b27d968bc056a
KostiantynU <115901013+KostiantynU@users.noreply.github.com>
2024-01-21 19:14:45 +0200

    Clean the code of custom component

--- src/components/PhoneBookForm/CustomSelect/CustomSelect.jsx
+++ src/components/PhoneBookForm/CustomSelect/CustomSelect.jsx
@@ -10,55 +10,16 @@ function CustomSelect(props) {
   const [selectedValue, setSelectedValue] = useState(value);
   const [displayUl, setDisplayUl] = useState('none');
 
-  const handleClickOnSelect = () => {
-    console.log('handleClickOnSelect');
-    setDisplayUl(!displayUl);
-  };
-
-  // const removeCustomListener = element => {
-  //   console.log('Removing event listener');
-  //   element.removeEventListener('click', addEventListenerForBody);
-  // };
-
-  const customEventListener = ({ event, bodyElement }) => {
-    console.log('event', event);
-    if (event.target.getAttribute('data-set-select') !== 'Select option') {
-      console.log('Not select option');
-      setDisplayUl('none');
-      // return bodyElement.removeEventListener('click', removeCustomListener);
-    }
-  };
-
-  // const addEventListenerForBody = ({ event, dataSetOfLi }) => {
-  //   const bodyElement = document.querySelector('body');
-  //   bodyElement.addEventListener('click', () => {});
-  //   console.log('event', event.target);
-  // if (event.target.getAttribute('data-set-select') !== dataSetOfLi) {
-  //   console.log('Not select option');
-  //   setDisplayUl('none');
-  //   return bodyElement.removeEventListener('click', removeCustomListener);
-  // }
-  // };
-
-  const handleClickOnCategory = async ({ event, item }) => {
-    const dataSetOfLi = event.target.getAttribute('data-set-select');
-    // console.log('dataSetOfLi', dataSetOfLi);
-    // addEventListenerForBody({ event, dataSetOfLi });
-
+  const handleClickOnCategory = async ({ item }) => {
     await setValue(item, false);
     setSelectedValue(item);
-    // removeCustomListener(bodyElement);
     setDisplayUl('none');
   };
 
   const selectOptions = ['All', 'Friends', 'Family', 'Colleagues'];
   return (
     <SelectWrapper
-      onClick={event => {
-        setDisplayUl(displayUl === 'none' ? 'block' : 'none');
-        const bodyElement = document.querySelector('body');
-        bodyElement.addEventListener('click', customEventListener({ event, bodyElement }));
-      }}
+      onClick={() => setDisplayUl(displayUl === 'none' ? 'block' : 'none')}
       $margin={props.$margin}
       $width={props.$width}
       $marginTop={props.$marginTop}
