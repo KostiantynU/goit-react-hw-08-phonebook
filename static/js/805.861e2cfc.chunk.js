be88e205d20dc7d178d509280841a27e92cf2b81
KostiantynU <115901013+KostiantynU@users.noreply.github.com>
2024-01-25 19:54:33 +0200

    Updates

---
+++ static/js/805.861e2cfc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_phonebook_ui=self.webpackChunkgoit_react_phonebook_ui||[]).push([[805],{9805:function(e,n,o){o.r(n);var t=o(3329);n.default=function(){return(0,t.jsx)(t.Fragment,{children:(0,t.jsx)("h3",{children:"Page did not found!"})})}}}]);
+//# sourceMappingURL=805.861e2cfc.chunk.js.map
